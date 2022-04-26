5b3f250e6a7bcc0ba4956038291caa9c76f299f2
julie <julietimmermans@ziggo.nl>
2022-04-26 12:01:47 +0200

    spelfout

--- src/main/java/MultiplyComputation.java
+++ src/main/java/MultiplyComputation.java
@@ -1,4 +1,4 @@
-public class MultiplyComplutation implements IComputation{
+public class MultiplyComputation implements IComputation{
 
     public int compute(int num1, int num2){
         return num1 * num2;
